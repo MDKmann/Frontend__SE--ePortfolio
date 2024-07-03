1c01293975cf91d175cadfcc7a126f2f436fc765
Michael <MDKuhlmann@zoho.com>
2024-07-03 09:46:25 -0500

    added tailwind config for scroll animation in tailwind.config.js, added animations and link templates in footer. Footer still in progress.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-// Tailwind CSS plugin activation file --> ignore file
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        keyframes: {
+            scroll: {
+                '0%': { transform: 'translateY(3px)'},
+                '100%': { transform: 'translateY(-3px)'}
+            }
+          },
+        animation: {
+            scroll: 'scroll 1s infinite alternate-reverse',
+          }
+      }
+    }
+  }
