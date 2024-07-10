6f1b464d423ba3eaa185593bbe7f1456f8827d23
Michael <MDKuhlmann@zoho.com>
2024-07-09 22:19:56 -0500

    added JavaScript modal function to contact button and mail sticky button to open and close modal contact page

--- index.js
+++ index.js
@@ -2,10 +2,34 @@
 // service_952vprc
 // 4GMijwLQzs35iRqNm
 
-function contact() {
+function contact(event) {
     event.preventDefault();
-//     console.log("it worked!");
-    setTimeout(() => {
-        console.log("it really worked!")
-    }, 500);
+    const loading = document.querySelector('#loading');
+    const success = document.querySelector('#success');
+    loading.classList += " z-10"
+    emailjs
+        .sendForm(
+            'service_952vprc',
+            'template_p99t2jl',
+            event.target,
+            '4GMijwLQzs35iRqNm'
+    ).then(() => {
+        loading.classList.remove('-z-10');
+        success.classList += " z-10";
+    }).catch(() => {
+        loading.classList.remove('-z-10');
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at MDKuhlmann@zoho.com"
+        );
+    })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
