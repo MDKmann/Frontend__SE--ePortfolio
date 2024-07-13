b72907bead751eee5abff832c826f970ae955c60
Michael <MDKuhlmann@zoho.com>
2024-07-13 13:55:47 -0500

    added temporary css to style.css for manual dark/light mode toggle selector due to tailwind css errors

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let isModeDark = false;
 function toggleContrast() {
     if (isModeDark) {
         isModeDark = false;
-        return documentElement.classList += " dark";
+        return document.documentElement.classList += " dark";
     }
     isModeDark = true;
-    return documentElement.classList.remove("dark");
+    return document.documentElement.classList.remove("dark");
 }
 
 function contact(event) {
@@ -44,3 +44,20 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += " modal--open";
 }
+
+
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//     document.documentElement.classList.add('dark')
+//   } else {
+//     document.documentElement.classList.remove('dark')
+//   }
+  
+//   // Whenever the user explicitly chooses light mode
+//   localStorage.theme = 'light'
+  
+//   // Whenever the user explicitly chooses dark mode
+//   localStorage.theme = 'dark'
+  
+//   // Whenever the user explicitly chooses to respect the OS preference
+//   localStorage.removeItem('theme')
